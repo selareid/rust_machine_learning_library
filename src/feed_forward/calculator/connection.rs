2d1b5f41ff2b613d06da8dbc2c7be7f0c6df9018
Daniel <danielselareid40@gmail.com>
2021-07-08 15:51:54 +1200

    Wrote tests for Connection methods

--- src/feed_forward/calculator/connection.rs
+++ src/feed_forward/calculator/connection.rs
@@ -30,3 +30,45 @@ impl PartialEq for Connection {
 }
 
 impl Eq for Connection {}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_connection_has_weight_and_node_given() {
+        let weight = 55_f64;
+        let node_ref = Rc::new(RefCell::new(Node::new(0.5)));
+
+        let new_connection = Connection::new(weight, Rc::clone(&node_ref));
+
+        assert_eq!(new_connection.weight, weight);
+        assert!(Rc::ptr_eq(&new_connection.node_ref, &node_ref));
+    }
+
+    #[test]
+    #[should_panic]
+    fn get_weighted_connection_value_panics_with_new_node() {
+        let weight = 55_f64;
+        let node_ref = Rc::new(RefCell::new(Node::new(0.5)));
+
+        let new_connection = Connection::new(weight, Rc::clone(&node_ref));
+
+        new_connection.get_weighted_connection_value();
+    }
+
+    #[test]
+    fn get_weighted_connection_value_returns_as_expected_with_doctored_node_output() {
+        let weight = 55_f64;
+        let node_ref = Rc::new(RefCell::new(Node::new(0.5)));
+
+        let node_output = 0.22_f64;
+        node_ref.borrow_mut().set_output(Some(node_output));
+
+        let new_connection = Connection::new(weight, Rc::clone(&node_ref));
+
+        let return_value = new_connection.get_weighted_connection_value();
+        assert_eq!(return_value, weight * node_output);
+    }
+}
